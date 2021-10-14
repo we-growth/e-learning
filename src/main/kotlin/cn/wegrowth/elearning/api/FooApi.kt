48b80643bab3465f0fc88e0b8669c8df51d4afc5
iCloud.Yin <xdyinay@qq.com>
2021-10-14 09:42:06 +0800

    这样写会有问题，分布式服务，无法获取用户的id.

--- src/main/kotlin/cn/wegrowth/elearning/api/FooApi.kt
+++ src/main/kotlin/cn/wegrowth/elearning/api/FooApi.kt
@@ -2,9 +2,11 @@ package cn.wegrowth.elearning.api
 
 import org.joda.time.DateTime
 import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.security.Principal
 
 @RestController
 @RequestMapping("api")
@@ -18,6 +20,12 @@ class FooApi {
     @PreAuthorize("hasRole('ADMIN')")
     fun getUserFoo(hello: String): String {
         return hello + " at " + DateTime.now()
+    }
+    @GetMapping("me")
+    @PreAuthorize("hasRole('ADMIN')")
+    fun getMe() : Any? {
+        val principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal()
+        return principal
     }
 
 }
